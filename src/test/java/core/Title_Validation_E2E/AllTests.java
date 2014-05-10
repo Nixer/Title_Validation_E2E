5cde8ebcc2ab6f613e1a15657aac348973cdf180
Nikolai Shegai <nixer1978@yahoo.com>
2014-05-09 21:38:07 -0700

    Initial commit

---
+++ src/test/java/core/Title_Validation_E2E/AllTests.java
@@ -0,0 +1,11 @@
+package core.Title_Validation_E2E;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ SeleniumTest.class })
+public class AllTests {
+
+}
